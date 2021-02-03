eff7ffa878ce64c6161e6f211cbf4bb227f5c172
Andrey <stepgt@mail.ru>
2021-02-03 18:05:39 +1000

    Create App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { list }
+    // Work without bind
+    // this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id
+    const updatedList = this.state.list.filter(isNotId)
+    this.setState({ list: updatedList })
+  }
+  render() {
+    return (
+      <div className="App">
+        { this.state.list.map( item =>
+          <div key={ item.objectID }>
+            <span>
+              <a href={ item.url }>{ item.title }</a>
+            </span>&nbsp;
+            <span>{ item.author }</span>&nbsp;
+            <span>{ item.num_comments }</span>
+            <span>{ item.points }</span>
+            &nbsp;
+            <span>
+              <button
+                onClick={ () => this.onDismiss(item.objectID) }
+                type="button"
+              >Отбросить
+              </button>
+            </span>
+          </div>
+        )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
