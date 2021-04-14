0c7cfdc1b4f50d6009611dff713e28da62cbb47f
Andrey <stepgt@mail.ru>
2021-04-15 04:02:13 +1000

    Added error to state

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null
     };
     // Work without bind
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -69,7 +70,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${HITSPERPAGE}${DEFAULT_COUNT}`)
       .then((response) => response.json())
       .then((result) => this.setSearchTopStories(result))
-      .catch((error) => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -126,11 +127,14 @@ class App extends Component {
 
   render() {
     console.log("render", this.state);
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
       (results && results[searchKey] && results[searchKey].hits) || [];
+    if (error) {
+      return <p>Что-то произошло не так.</p>;
+    }
     return (
       <div className="page">
         <div className="interactions">
@@ -142,10 +146,13 @@ class App extends Component {
             Поиск
           </Search>
         </div>
-        <Table
-          list={list}
-          onDismiss={this.onDismiss}
-        />
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             Больше историй
