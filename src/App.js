14461f0d9bbd8b547f14173c024417fa7e86e23f
Andrey <stepgt@mail.ru>
2021-04-28 09:08:00 +1000

    Added PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import PropTypes from 'prop-types'
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
@@ -201,6 +202,25 @@ const Button = ({ onClick, className, children }) => {
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
 };
 
 export default App;
