b7fc15d18dfdc14e908090b8f567d1d00e23082b
Andrey <stepgt@mail.ru>
2021-05-07 07:15:27 +1000

    Refactor setSearchTopStories

--- src/App.js
+++ src/App.js
@@ -31,6 +31,18 @@ const smallColumn = {
   width: "10%",
 };
 
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+  const { searchKey, results } = prevState;
+  const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+  const updatedHits = [...oldHits, ...hits];
+  return {
+    results: {
+      ...results,
+      [searchKey]: { hits: updatedHits, page },
+    },
+    isLoading: false,
+  };
+};
 
 class App extends Component {
   constructor(props) {
@@ -59,16 +71,7 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page } = result;
-    const { searchKey, results } = this.state;
-    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
-    const updatedHits = [...oldHits, ...hits];
-    this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      },
-      isLoading: false
-    });
+    this.setState(updateSearchTopStoriesState(hits, page));
   }
 
   onSearchSubmit(e) {
